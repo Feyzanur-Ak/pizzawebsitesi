96eebbd971eb9769fd6ed3e4b5df140425384727
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2024-11-29 17:55:51 +0300

    Ana Syfa

--- components/AnaSayfa.jsx
+++ components/AnaSayfa.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './AnaSayfa.css';
+import { useHistory } from "react-router-dom";
 
 const AnaSayfa = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default AnaSayfa
+
+    const handleClick = () => {
+        
+
+    }
+
+  return (
+    <main className="AnaSayfa-container">
+      <header className="AnaSayfa-header">
+        <h2>Teknolojik Yemekler</h2>
+        <h1>
+          KOD ACIKTIRIR <br /> PIZZA, DOYURUR
+        </h1>
+      </header>
+      <button onClick={handleClick} className="AnaSayfa-button" aria-label="Acıkınca Tıklayın">
+        ACIKTIM
+      </button>
+    </main>
+  );
+};
+
+export default AnaSayfa;
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
+import React from 'react';
+import AnaSayfa from '../components/AnaSayfa'; 
+import '../components/AnaSayfa.css'; 
+import '../index.css';
 
 function App() {
- 
-
   return (
-    <>
-    
-
-     
-    </>
-  )
+    <div>
+      <AnaSayfa />
+    </div>
+  );
 }
 
-export default App
+export default App;
