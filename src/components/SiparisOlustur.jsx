35b2d9435b3b2247dd977442f79ae65693ed77e1
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2024-12-03 22:00:42 +0300

    iteratiom-2/siparişoluştur

--- src/components/SiparisOlustur.jsx
+++ src/components/SiparisOlustur.jsx
@@ -532,6 +532,7 @@ const SiparisOlustur = () => {
           />
         </InputContainer>
 
+            <hr  style={{ width: "100%", height: ".5px", backgroundColor: "#ffffff", margin: " 10px 20px"}}/>
         <SummarySection>
           <QuantityControl>
             <QuantityButton onClick={() => handleQuantityChange("decrement")}>
