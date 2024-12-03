43220bbb08361105c2952051d3b3c9afcd9c3087
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2024-12-03 10:07:22 +0300

    Menu-Section-1 100/90

--- src/components/MenuSection.jsx
+++ src/components/MenuSection.jsx
@@ -9,71 +9,94 @@ import nav6 from "../pictures/6.svg";
 import kart1 from "../pictures/kart-1.png";
 import kart2 from "../pictures/kart-2.png";
 import kart3 from "../pictures/kart-3.png";
-import { Container } from "reactstrap";
+import { Container } from "reactstrap"; // Eğer reactstrap yoksa npm install reactstrap komutunu çalıştırın
+
+// Styled Components
 
 const Navbar = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-evenly;
   align-items: center;
-  padding: 10px;
+  padding: 0 200px;
   margin: 10px;
   font-family: "Roboto Condensed", sans-serif;
+
+  p {
+    display: flex;
+    align-items: center;
+    gap: 10px; 
+    font-size: 16px;
+    cursor: pointer;
+
+    img {
+      width: 35px;
+      height: 30px;
+    }
+  }
 `;
 
 const ImageSection = styled.div`
-display: flex;
-justify-content: space-around;
-gap:-50px;
-  padding: 20px;
-  align-items: center;
+  display: flex;
+  gap:10px;
+  padding: 100px 150px;
+ justify-content:center;
+ align-items: center;
+background-color: #faf7f2;
 `;
 
 const ImageContainer = styled.div`
   background-color: #ce2829;
-  border-radius: 10px;
   position: relative;
-  padding: 20px;
+  width: 40%; 
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  color: white;
+  justify-content:space-around
 
-  img {
-    border-radius: 0 0 10px 10px;
-    width: 100%;
-    height: auto;
-    object-fit: cover;
-  }
+
 `;
 
 const RightImages = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 20px;
+   width: 40%; 
+  border:none;
+  position: relative; 
 
-  img {
-    border-radius: 10px;
-    width: 100%;
-    height: auto;
-    object-fit: cover;
-  }
+    img {
+      border-radius: 10px;
+      width: 100%;
+      object-fit: cover;
+      margin:5px;
+    }
+  
 `;
 
 const Content = styled.div`
   position: absolute;
   top: 20px;
-  left: 20px;
+  left: 30px;
+  color:white;
 
-  h2 {
-    font-size: 36px;
-    font-weight: 600;
+  h1 {
+    font-size: 3rem;
+    font-weight: bold;
     margin-bottom: 10px;
   }
 
   p {
     font-size: 16px;
     margin-bottom: 20px;
+    color:white;
   }
+`;
+
+const Content1=styled.div`
+  position: absolute;
+  left: 40px;
+  padding-top: 20px;
+  color:black;
+  background-color:transparent
+
 `;
 
 const Button = styled.button`
@@ -81,19 +104,19 @@ const Button = styled.button`
   color: #ce2829;
   font-size: 14px;
   font-weight: bold;
-  padding: 10px 20px;
+  padding: 15px 20px;
   border: none;
   border-radius: 20px;
   cursor: pointer;
 
-  &:hover {
-    background-color: #f9f5f0;
-  }
 `;
+
+// React Component
 
 const MenuSection = () => {
   return (
     <Container>
+      {/* Navigation Bar */}
       <Navbar>
         <p>
           <img src={nav1} alt="nav1" /> YENİ! Kore
@@ -114,18 +137,33 @@ const MenuSection = () => {
           <img src={nav6} alt="nav6" /> Gazlı İçecekler
         </p>
       </Navbar>
+
+      {/* Main Image Section */}
       <ImageSection>
+        {/* Left Block */}
         <ImageContainer>
+        <img src={kart1} alt="Pizza" />
           <Content>
-            <h2>Özel Lezzetus</h2>
+            <h1>Özel <br /> Lezzetus</h1>
             <p>Position: Absolute Acı Burger</p>
             <Button>SİPARİŞ VER</Button>
           </Content>
-          <img src={kart1} alt="Pizza" />
         </ImageContainer>
+
+     
         <RightImages>
-          <img src={kart2} alt="Hackathlon" />
-          <img src={kart3} alt="Kurye" />
+          <div>
+            <Content1>
+              <h2 style={{ color: "white" }}>Hackathlon <br /> Burger Menü</h2>
+              <Button>SİPARİŞ VER</Button>
+            </Content1>
+            <img src={kart2} alt="Hackathlon" />
+              <Content1>
+              <h2> <span style={{ color: "red" }}>Çooook</span> Hızlı <br /> npm gibi kurye</h2>
+              <Button>SİPARİŞ VER</Button>
+            </Content1>
+            <img src={kart3} alt="Kurye" />
+          </div>
         </RightImages>
       </ImageSection>
     </Container>
