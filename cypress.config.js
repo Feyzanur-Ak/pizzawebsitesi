55bc31bb985f013a9c82c21b328b5c298852ba26
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2024-12-05 10:42:53 +0300

    cypress deneme

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.cy.js", // Adjust this to where your test files are located
     baseUrl: 'http://localhost:5173',
   },
-
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "webpack",
-    },
-  },
-});
-
+})
